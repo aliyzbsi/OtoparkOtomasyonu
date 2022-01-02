cebc0521adab37f5c8f106178887b725a0b230af
EmreCanKaynar <sukaynaremre@gmail.com>
2022-01-02 20:35:01 +0300

    deneme1

--- OtoparkOtomasyonu/OtoparkOtomasyonu/FormAdminAnasayfa.Designer.cs
+++ OtoparkOtomasyonu/OtoparkOtomasyonu/FormAdminAnasayfa.Designer.cs
@@ -29,34 +29,62 @@ namespace OtoparkOtomasyonu
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.buttonGirisYAP = new System.Windows.Forms.Button();
+            this.labelbaslik = new System.Windows.Forms.Label();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
             this.SuspendLayout();
             // 
-            // button1
+            // buttonGirisYAP
             // 
-            this.button1.Location = new System.Drawing.Point(552, 175);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonGirisYAP.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonGirisYAP.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonGirisYAP.ForeColor = System.Drawing.SystemColors.Control;
+            this.buttonGirisYAP.Location = new System.Drawing.Point(282, 295);
+            this.buttonGirisYAP.Name = "buttonGirisYAP";
+            this.buttonGirisYAP.Size = new System.Drawing.Size(166, 58);
+            this.buttonGirisYAP.TabIndex = 0;
+            this.buttonGirisYAP.Text = "GİRİŞ YAP";
+            this.buttonGirisYAP.UseVisualStyleBackColor = false;
+            this.buttonGirisYAP.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelbaslik
+            // 
+            this.labelbaslik.AutoSize = true;
+            this.labelbaslik.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labelbaslik.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelbaslik.Location = new System.Drawing.Point(247, 40);
+            this.labelbaslik.Name = "labelbaslik";
+            this.labelbaslik.Size = new System.Drawing.Size(262, 36);
+            this.labelbaslik.TabIndex = 1;
+            this.labelbaslik.Text = "ADMİN GİRİŞ YAP";
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.ımageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
             // 
             // FormAdminAnasayfa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelbaslik);
+            this.Controls.Add(this.buttonGirisYAP);
             this.Name = "FormAdminAnasayfa";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonGirisYAP;
+        private System.Windows.Forms.Label labelbaslik;
+        private System.Windows.Forms.ImageList ımageList1;
     }
 }
 
